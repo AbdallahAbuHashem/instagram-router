3c4f9464a698ffafa12191705a27e3f2ed1d08eb
AbdallahAbuHashem <aabuhash@stanford.edu>
2021-06-29 08:22:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import logo from './logo.svg';
+import { Grid, Typography } from '@material-ui/core'
+import { MoreHoriz } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
+
+import profilepic from './assets/pfp.jpg';
+import contentpic from './assets/content.jpg'
 import './App.css';
 
-function App() {
+
+const useStyles = makeStyles(theme => ({
+  profileInfo: {
+    flex: 1,
+  },
+  profilepic: {
+    borderRadius: 16,
+    marginRight: theme.spacing(1),
+  }
+}));
+
+function Home() {
+  const classes = useStyles();
+  const username = "username";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="row">
+        <Grid xs={2} item>
+
+        </Grid>
+        <Grid xs={8} item container direction="column" alignItems="stretch">
+          <Grid xs={12} item container direction="row" alignItems="center" justifyContent="space-between" width="100%">
+            <img src={profilepic} width={32} height={32} className={classes.profilepic}/>
+            <Grid item container direction="column" alignItems="flex-start" justifyContent="space-between" className={classes.profileInfo}>
+              
+              <Typography variant="body2"><Link to={`/user/${username}`}>Profile name</Link></Typography>
+              <Typography variant="caption">Gaza, Palestine</Typography>
+            </Grid>
+            
+            <MoreHoriz></MoreHoriz>
+          </Grid>
+          <img src={contentpic} width="100%"/>
+        </Grid>
+        <Grid xs={2} item>
+
+        </Grid>
+      </Grid>
+  )
+}
+
+function User() {
+  const { id } = useParams()
+  return (
+    <div>{id}</div>
+  )
+}
+
+function App() {
+  const classes = useStyles();
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/user/:id">
+            <User />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
